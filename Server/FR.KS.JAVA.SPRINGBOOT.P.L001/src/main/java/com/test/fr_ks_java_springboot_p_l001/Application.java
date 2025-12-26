8845ed4f33352f07131d17909f498bc37eb7e224
quypdhe173508 <quypdhe173508@fpt.edu.vn>
2025-12-26 14:51:10 +0700

    feat: sekeleton backend

---
+++ Server/FR.KS.JAVA.SPRINGBOOT.P.L001/src/main/java/com/test/fr_ks_java_springboot_p_l001/Application.java
@@ -0,0 +1,13 @@
+package com.test.fr_ks_java_springboot_p_l001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ Server/FR.KS.JAVA.SPRINGBOOT.P.L001/src/test/java/com/test/fr_ks_java_springboot_p_l001/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.fr_ks_java_springboot_p_l001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
